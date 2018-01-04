e369abca8f29742b45b0ef7b0ea06e333813a51a
Will Johnson <wijohns@microsoft.com>
2018-01-04 15:35:39 -0600

    Added script for deletion of resource group

---
+++ scripts/Delete-ResourceGroup.ps1
@@ -0,0 +1,6 @@
+﻿# Delete a resource group and all contents in it
+# Holds the powershell script until complete
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/powershell-azure-resource-manager
+
+# Use -Force option to prevent prompting
+Remove-AzureRmResourceGroup -Name "ResourceGroupName"
