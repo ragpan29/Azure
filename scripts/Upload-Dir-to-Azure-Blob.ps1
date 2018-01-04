d322f72ece8e6a595927b2de880efbe9c8b19599
Will Johnson <wijohns@microsoft.com>
2018-01-04 09:56:03 -0600

    Added uploading a directory of files to blob script

---
+++ scripts/Upload-Dir-to-Azure-Blob.ps1
@@ -0,0 +1,26 @@
+﻿# Upload data to Azure Blob Storage
+# https://docs.microsoft.com/en-gb/azure/hdinsight/hdinsight-upload-data
+# Log in to the Azugre
+Login-AzureRmAccount
+
+$resourceGroupName = "<AzureResourceGroupName>"
+$storageAccountName = "<StorageAccountName>"
+$containerName = "<ContainerName>"
+
+$filepath ="<LocalFileName>"
+
+
+# Get the storage account key
+$storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+# Create the storage context object
+$destContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageaccountkey
+
+# Iterate through every
+Get-ChildItem $fp |
+Foreach-Object {
+    $blobName = $_.Name
+    # Copy the file from local workstation to the Blob container
+    # Use -Force to force overwrite and avoid being prompted
+    Set-AzureStorageBlobContent -File $_.FullName -Container $containerName -Blob $blobName -context $destContext -Force
+}
+
