fc2df48dd091000f096810491673b7acdc38e458
Will Johnson <wijohns@microsoft.com>
2018-09-24 16:44:10 -0500

    Added Speech to Text and Translation C# application

---
+++ cogsvcs/speech_to_text_translation_c_sharp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using System.Net.Http;
+using System.Text;
+// NOTE: Install the Newtonsoft.Json NuGet package.
+using Newtonsoft.Json;
+
+namespace SpeechToText
+{
+    class Program
+    {
+        static string host = "https://api.cognitive.microsofttranslator.com";
+        static string path = "/translate?api-version=3.0";
+        // Translate to German and Italian.
+        static string params_ = "&to=de&to=it";
+
+        static string uri = host + path + params_;
+
+        // NOTE: Replace this example key with a valid subscription key.
+        static string key = "XXXXXXXXXXXXXXXXXXXXXXXX";
+
+        async static void Translate(string text)
+        {
+            System.Object[] body = new System.Object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+
+                Console.OutputEncoding = UnicodeEncoding.UTF8;
+                Console.WriteLine(result);
+            }
+        }
+
+
+        public static async Task RecognizeSpeechAsync()
+        {
+            // Creates an instance of a speech factory with specified
+            // subscription key and service region. Replace with your own subscription key
+            // and service region (e.g., "westus").
+            var factory = SpeechFactory.FromSubscription("XXXXXXXXXXXXXXXXXX", "westus");
+
+            // Creates a speech recognizer.
+            using (var recognizer = factory.CreateSpeechRecognizer())
+            {
+                Console.WriteLine("Say something...");
+
+                // Performs recognition.
+                // RecognizeAsync() returns when the first utterance has been recognized, so it is suitable 
+                // only for single shot recognition like command or query. For long-running recognition, use
+                // StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeAsync();
+
+                // Checks result.
+                if (result.RecognitionStatus != RecognitionStatus.Recognized)
+                {
+                    Console.WriteLine($"Recognition status: {result.RecognitionStatus.ToString()}");
+                    if (result.RecognitionStatus == RecognitionStatus.Canceled)
+                    {
+                        Console.WriteLine($"There was an error, reason: {result.RecognitionFailureReason}");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No speech could be recognized.\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                    Translate(result.Text);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            var active = true;
+            while (active)
+            {
+                RecognizeSpeechAsync().Wait();
+                Console.WriteLine("Please press a key to continue.");
+                var capture = Console.ReadLine();
+                if (capture == "exit"){
+                    active = false;
+                }
+            }
+        }
+
+    }
+}
---
+++ cogsvcs/speech_to_text_translation_c_sharp/ReadMe.md
@@ -0,0 +1,9 @@
+# Short Speech to Text and Translation
+
+This demo provides a C# console application to let you speak into your microphone and have it transcribed.  With that transcription, the Translation Service will pickup the text and translate to German and Italian.  The Speech to Text part is based on this [quickstart](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/quickstart-csharp-dotnet-windows).
+
+You will need to...
+
+ * Deploy the Azure Speech Service
+ * Deploy the Azure Translation Service
+ * Build the solution found here.
